bc59507fab72d73a64d801b98f726eb77abaeb38
Aykhan Z. Dadashov <AykhanZd@CODE.LOCAL>
2019-10-12 19:56:36 +0400

    added

--- CarCreditApp/js/card.js
+++ CarCreditApp/js/card.js
@@ -1,6 +1,16 @@
 let car = localStorage['cardclicked'];
 let newkey = JSON.parse(car);
-
-let page = document.getElementById('page');
-
-page.innerText=newkey.price;
+let table = document.getElementsByClassName('ch')[0]
+table.className = "table"
+let massive = ['Şəhər', 'Marka', 'Model', 'Buraxılış ili', 'Ban növü', 'Rəngi', 'Mühərrikin həcmi', 'Mühərrikin gücü', 'Yanacaq növü', 'Yürüş', 'Sürətlər qutusu', 'Ötürücü', 'Yeni', 'Qiymət']
+for (let i in newkey) {
+    let a = document.createElement("tr");
+    let b = document.createElement("td");
+    let c = document.createElement("td");
+    b.className = "aa"
+    table.appendChild(a);
+    b.innerText=i;
+    c.innerText = newkey[i];
+    a.appendChild(b);
+    a.appendChild(c);
+}
